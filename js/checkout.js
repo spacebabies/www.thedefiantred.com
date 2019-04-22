0afc907e4b6e349e53278e4be1be73116a1f36fc
Joost Baaij <joost@spacebabies.nl>
2019-04-22 23:54:15 +0200

    make checkout dynamic

--- js/checkout.js
+++ js/checkout.js
@@ -1,27 +1,19 @@
-var stripe = Stripe('608d8d612972e031398d4a1c110f578e39ede630');
+var stripe = Stripe('pk_live_9e0LBOYyCmPlVcxYSrNricJe00aPuFrmK2');
+var checkoutButtons = document.querySelectorAll('.item-plan button')
+for(var i=0; i<checkoutButtons.length; i++) {
+  var checkoutButton = checkoutButtons[i];
 
-var checkoutButton = document.getElementById('checkout-button-sku_EveseRWsvXPnTG');
-
-if (checkoutButton) {
   checkoutButton.addEventListener('click', function () {
-    // When the customer clicks on the button, redirect
-    // them to Checkout.
     stripe.redirectToCheckout({
-      items: [{sku: 'sku_EveseRWsvXPnTG', quantity: 1}],
-
-      // Note that it is not guaranteed your customers will be redirected to this
-      // URL *100%* of the time, it's possible that they could e.g. close the
-      // tab between form submission and the redirect.
+      items: [{sku: checkoutButton.value, quantity: 1}],
       successUrl: 'https://www.thedefiantred.com/success',
       cancelUrl: 'https://www.thedefiantred.com/canceled',
     })
     .then(function (result) {
       if (result.error) {
-        // If `redirectToCheckout` fails due to a browser or network
-        // error, display the localized error message to your customer.
         var displayError = document.getElementById('error-message');
         displayError.textContent = result.error.message;
       }
     });
-  });  
+  });
 }
