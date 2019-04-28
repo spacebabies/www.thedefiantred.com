96ccf71101a48ff260f922e76c807a16f52e2000
Joost Baaij <joost@spacebabies.nl>
2019-04-28 22:23:53 +0200

    try to improve the Stripe event handler

--- js/checkout.js
+++ js/checkout.js
@@ -1,11 +1,9 @@
 var stripe = Stripe('pk_live_9e0LBOYyCmPlVcxYSrNricJe00aPuFrmK2');
 var checkoutButtons = document.querySelectorAll('.item-plan button')
 for(var i=0; i<checkoutButtons.length; i++) {
-  var checkoutButton = checkoutButtons[i];
-
-  checkoutButton.addEventListener('click', function () {
+  checkoutButtons[i].addEventListener('click', function () {
     stripe.redirectToCheckout({
-      items: [{sku: checkoutButton.value, quantity: 1}],
+      items: [{sku: this.value, quantity: 1}],
       successUrl: 'https://www.thedefiantred.com/#stripe-success',
       cancelUrl: 'https://www.thedefiantred.com/#stripe-canceled',
     })
